76de6994b30d524f253f05f4c41203a3d4026d42
burak.yildirim <burak_yildirim_official@hotmail.com>
2024-07-17 16:15:14 +0300

    add fastlane commands for upload binary

--- fastlane/scripts.sh
+++ fastlane/scripts.sh
@@ -1,2 +1,9 @@
 ## for metadata update/upload
 fastlane supply --skip_upload_apk true --skip_upload_aab true --changes_not_sent_for_review true --sync_image_upload true  --version_code "121236"
+
+## for upload binary (aab)
+fastlane supply --track_promote_release_status "draft" --release_status "draft" --skip_upload_apk true --skip_upload_aab false --skip_upload_metadata true --skip_upload_changelogs true --skip_upload_images true --skip_upload_screenshots true --aab "AAB_PATH"
+
+## for upload binary (apk)
+fastlane supply --track_promote_release_status "draft" --release_status "draft" --skip_upload_apk false --skip_upload_aab true --skip_upload_metadata true --skip_upload_changelogs true --skip_upload_images true --skip_upload_screenshots true --apk "APK_PATH" 
+
